a1cbf097384630fc5059fa037c0109acecf60668
websupporter <webmaster@websupporter.net>
2016-06-07 11:33:57 +0300

    Uploading plugin

---
+++ export-wpseo.php
@@ -0,0 +1,89 @@
+<?php
+	/*
+	 Plugin Name: Export wpSEO
+	 Description: Export wpSEO data using the SEO data transporter
+	 Plugin URI: https://github.com/KrautPress/export-wpseo
+	 Text Domain: export-wpseo
+	 Version: 0.1
+	 Tags: wpseo, seo, export, import
+	 Contributors: krafit, websupporter
+	 Author URI: https://krautpress.de
+	 License: GPLv2 or later
+	 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+	 Export wpSEO is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 2 of the License, or
+	 any later version.
+
+	 Export wpSEO is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 GNU General Public License for more details.
+ 
+	 You should have received a copy of the GNU General Public License
+	 along with Export wpSEO. If not, see http://www.gnu.org/licenses/gpl-2.0.html.
+	 */
+
+	/**
+	 * Adds wpSEO to the SEO data transporter
+	 **/
+	add_action( 'seodt_init', 'add_wpseo_to_seo_data_transporter' );
+	function add_wpseo_to_seo_data_transporter() {
+		global $_seodt_themes, $_seodt_plugins, $_seodt_platforms;
+
+		$_seodt_plugins['wpSEO'] = array(
+			'Custom Doctitle'        => '_wpseo_edit_title',
+			'META Description'       => '_wpseo_edit_description',
+			'Open Graph Title'       => '_wpseo_edit_og_title',
+			'Open Graph Description' => '_wpseo_edit_og_description',
+			'Open Graph Image'       => '_wpseo_edit_og_image',
+			'Twitter Title'          => '_wpseo_edit_twittercard_title',
+			'Twitter Description'    => '_wpseo_edit_twittercard_description',
+			'Twitter Image'          => '_wpseo_edit_twittercard_image',
+			'Canonical URI'          => '_wpseo_edit_canonical',
+			'Redirect URI'           => '_wpseo_edit_redirect',
+		);
+
+		//Add open graph title/description/image to Yoast WordPress SEO
+		$_seodt_plugins['WordPress SEO']['Open Graph Title']       = '_yoast_wpseo_opengraph-title';
+		$_seodt_plugins['WordPress SEO']['Open Graph Description'] = '_yoast_wpseo_opengraph-description';
+		$_seodt_plugins['WordPress SEO']['Open Graph Image']       = '_yoast_wpseo_opengraph-image';
+
+		//Add twitter title/description/image to Yoast WordPress SEO
+		$_seodt_plugins['WordPress SEO']['Twitter Title']       = '_yoast_wpseo_twitter-title';
+		$_seodt_plugins['WordPress SEO']['Twitter Description'] = '_yoast_wpseo_twitter-description';
+		$_seodt_plugins['WordPress SEO']['Twitter Image']       = '_yoast_wpseo_twitter-image';
+		$_seodt_platforms = array_merge( $_seodt_themes, $_seodt_plugins );
+
+	}
+
+	/**
+	 * Checks on activation, if the SEO data transporter is acitiavted
+	 * and returns an admin notice, if it's not activated
+	 **/
+	register_activation_hook( __FILE__, 'add_wpseo_on_activation' );
+	function add_wpseo_on_activation() {
+		if ( ! is_plugin_active( 'seo-data-transporter/plugin.php' ) ) {
+			update_option( 'add_wpseo_needs_seo_data_transporter_notice', 1 );
+		}
+	}
+
+	add_action( 'admin_init', 'add_wpseo_needs_seo_data_transporter_notice_deferred' );
+	function add_wpseo_needs_seo_data_transporter_notice_deferred(){
+		if( ! get_option( 'add_wpseo_needs_seo_data_transporter_notice' ) )
+			return;
+
+		delete_option( 'add_wpseo_needs_seo_data_transporter_notice' );
+		add_action( 'admin_notices', 'add_wpseo_needs_seo_data_transporter_notice' );
+	}
+
+	function add_wpseo_needs_seo_data_transporter_notice() {
+		?>
+			<div class="notice notice-warning is-dismissible">
+				<p><?php _e( 'The Plugin SEO Data Transporter needs to be active in order for Add wpSEO to run.', 'export-wpseo' ); ?></p>
+			</div>
+		<?php
+	}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Export wpSEO ===
+Contributors: krafit,websupporter
+Tags: wpseo, seo, export, import
+Requires at least: 4.1
+Tested up to: 4.5
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds wpSEO to the "SEO data transporter" to easily export SEO data to other SEO plugins
+
+== Description ==
+The SEO data transporter plugin enables you to easily switch between SEO plugins. This plugin helps you to transport your SEO data from one plugin to another. Unfortunatly the popular wpSEO plugin was not supported by this plugin - yet. Export wpSEO extends now the functionality of the transporter plugin and adds the necessary information to export the data of the wpSEO plugin.
+
+== Installation ==
+1. Install the <a href="https://wordpress.org/plugins/seo-data-transporter/">SEO data transporter plugin</a> and activate it.
+2. Install the Export wpSEO plugin and activate it.
+3. If you switch now to Tools > SEO data transport you should see wpSEO as anothe available plattform
+
+== Frequently Asked Questions ==
+Can I use this plugin without the transporter plugin?
+No.
+
+What is wpSEO?
+It is a well known SEO plugin.
+
+== Changelog ==
+0.1
+
+Initial release.
